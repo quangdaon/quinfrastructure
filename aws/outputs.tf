4c7606be4587ea093fc32f4cdca4c90c60cb7c5a
quangdaon <me@quangdao.com>
2024-11-24 22:07:54 -0600

    Add security group output

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -12,3 +12,8 @@ output "alb_arn" {
   description = "ARN of the load balancer"
   value       = module.load_balancer.arn
 }
+
+output "alb_security_group" {
+  description = "ID of the ALB security group"
+  value       = module.load_balancer.security_group
+}
